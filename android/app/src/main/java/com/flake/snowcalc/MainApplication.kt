d3b6397f1c9e08c34fb1230a0ec77a237750cb70
Zoran Jeremić <batajeremiccc@gmail.com>
2025-08-25 13:14:29 +0200

    Create MainApplication.kt

---
+++ android/app/src/main/java/com/flake/snowcalc/MainApplication.kt
@@ -0,0 +1,15 @@
+package com.flake.snowcalc
+
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainApplication : FlutterApplication(), PluginRegistry.PluginRegistrantCallback {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun registerWith(registry: PluginRegistry?) {
+        GeneratedPluginRegistrant.registerWith(registry)
+    }
+}
